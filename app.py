749dc3759d387d196b930f553fde12cc45d00247
jayblaine <$h@d0wb18DE>
2023-02-12 13:48:31 -0600

    files read

--- app.py
+++ app.py
@@ -419,7 +419,7 @@ def build_app():
         # flow_sniffer.sniffer.start()
         follow_thread = threading.Thread(target=alert_follow, name="alert_follower")
         # follow_thread.start()
-        app.run(port=80, debug=False)
+        #app.run(port=80, debug=False)
     except KeyboardInterrupt:
         print('exiting')
         # flow_sniffer.sniffer.stop()
