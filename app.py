c9247f2283b3245907c02de7db34c210870401ef
jayblaine <$h@d0wb18DE>
2023-02-12 14:12:57 -0600

    files read

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from netaddr import IPNetwork
 from webApp import forms
 from webApp import flow_tracker
 from webApp import t_flows
-from webApp.follow import alert_follow
 
 app = Flask(__name__)
 app.config['SESSION_COOKIE_SAMESITE'] = "Secure"
@@ -24,6 +23,41 @@ app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Ld81k4kAAAAAHaEuoxKtg7N2QE11yjP3ySy8X-U'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6Ld81k4kAAAAANDMNw2lbt5hzjXg71XbErsN37S3'
 # flow_sniffer.sniffer.start()
+
+
+def follow(file):
+    """
+    Behaves like tail -f: follows file and returns new lines as they're appended
+    :param file: followed file
+    :return: yields lines as they're appended, continues execution.
+    """
+    file.seek(0, 2)
+    while True:
+        line = file.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+def alert_follow():
+    alert_file = open('/mnt/captures/snort_internal/alert', "r")
+    alert_lines = follow(alert_file)
+
+    for line in alert_lines:
+        line_list = line.split()
+        src = line_list[-3:][0]
+        dst = line_list[-1:][0]
+        try:
+            flow_sniffer.flows["{} {}".format(src, dst)].label = 1
+            flow_sniffer.flows["{} {}".format(src, dst)].flow_alert = re.split(r'\[\*\*]', line)[1]
+        except KeyError:
+            try:
+                flow_sniffer.flows["{} {}".format(dst, src)].label = 1
+                flow_sniffer.flows["{} {}".format(src, dst)].flow_alert = re.split(r'\[\*\*]', line)[1]
+            except KeyError:
+                # Key error: Some snort rules (port sweep) don'flow_sniffer have port number -> can'flow_sniffer find reliably
+                continue
 
 
 app = None
--- follow.py
+++ follow.py
@@ -3,36 +3,3 @@ import time
 from webApp.app import flow_sniffer
 
 
-def follow(file):
-    """
-    Behaves like tail -f: follows file and returns new lines as they're appended
-    :param file: followed file
-    :return: yields lines as they're appended, continues execution.
-    """
-    file.seek(0, 2)
-    while True:
-        line = file.readline()
-        if not line:
-            time.sleep(0.1)
-            continue
-        yield line
-
-
-def alert_follow():
-    alert_file = open('/mnt/captures/snort_internal/alert', "r")
-    alert_lines = follow(alert_file)
-
-    for line in alert_lines:
-        line_list = line.split()
-        src = line_list[-3:][0]
-        dst = line_list[-1:][0]
-        try:
-            flow_sniffer.flows["{} {}".format(src, dst)].label = 1
-            flow_sniffer.flows["{} {}".format(src, dst)].flow_alert = re.split(r'\[\*\*]', line)[1]
-        except KeyError:
-            try:
-                flow_sniffer.flows["{} {}".format(dst, src)].label = 1
-                flow_sniffer.flows["{} {}".format(src, dst)].flow_alert = re.split(r'\[\*\*]', line)[1]
-            except KeyError:
-                # Key error: Some snort rules (port sweep) don'flow_sniffer have port number -> can'flow_sniffer find reliably
-                continue
