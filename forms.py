8243d1b0cf2662806b7934e003e1a41e7c7fbdb9
jayblaine <$h@d0wb18DE>
2023-02-12 02:08:26 -0600

    t

--- forms.py
+++ forms.py
@@ -1,4 +1,21 @@
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import (StringField, EmailField, SelectMultipleField, widgets)
+from wtforms.validators import InputRequired, Length
 
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 
+class EmailForm(FlaskForm):
+    first_name = StringField('First Name', validators=[InputRequired(),
+                                             Length(min=2, max=100)])
+    last_name = StringField('Last Name', validators=[InputRequired(),
+                                                  Length(min=2, max=100)])
+    org = StringField('Organization/University', validators=[InputRequired(),
+                                                     Length(min=2, max=100)])
+    email = EmailField('Email', validators=[InputRequired()])
+    interest = MultiCheckboxField('Interest',
+                       choices=['More Information', 'Data Sharing', 'Collaboration'])
+    recaptcha = RecaptchaField()
