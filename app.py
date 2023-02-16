1041a13b9656f4b934ca03cd6cff5db7d23d5e2b
jayblaine <$h@d0wb18DE>
2023-02-16 11:33:13 -0600

    network layout update

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def create_dash_micro(flask_app):
                                                     'solver': 'barnesHut',
                                                     'barnesHut': {
                                                         'theta': 0.4,
-                                                        'gravitationalConstant': -2000,
+                                                        'gravitationalConstant': -1000,
                                                         'centralGravity': 0.3,
                                                         'springLength': 95,
                                                         'springConstant': 0.04,
