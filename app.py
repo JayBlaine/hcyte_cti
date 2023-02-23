f4e6490f286d65bc462a86b813b10185c219d559
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-02-23 13:50:39 -0600

    Scan finder test

--- app.py
+++ app.py
@@ -278,6 +278,37 @@ def build_visdcc(n_intervals=None, live_check=None, vis_filter=None, proto_filte
                     if visdcc_display_dict[active_int][key].ip_proto in proto_switches:
                         edges.append(new_edge)
 
+    #search for potential scans by checking for edges with matching to and from IPs
+    scans = {}
+    #focus on one edge
+    for i in range(edges):
+        matches = 0
+        #loop through the other edges
+        for edge in edges:
+            #increment matches if both the to and from IPs match
+            if(edges[i]["from"] == edge["from"] and edges[i]["to"] == edge["to"]):
+                print("Scan match incremented")
+                matches += 1
+        #if the required amount of matches is reached, add the to and from IPs as a potential scan 
+        if(matches > 5): #5 is just an arbitrary threshold
+            print("Scan found") 
+            scans[edges[i]["from"]] = edges[i]["to"]
+
+    #search for potential sweeps within the edges
+    sweeps = {}
+    for i in range(edges):
+        matches = 0
+        #get the dest port by spliting the edge id
+        cur_dest = edges[i]["id"].split("__")[1].split(":")[1]
+        for edge in edges:
+            edge_dest = edge["id"].split("__")[1].split(":")[1]
+            #if the from IPs and dest ports match, it is a potential scan
+            if(edges[i]["from"] == edge["from"] and cur_dest == edge_dest):
+                matches += 1
+        if(matches > 5):
+            sweeps[edges[i]["from"]] = cur_dest
+    
+    
     ip_all = set(srcIPs + destIPs)
 
     for ip in ip_all:  # nodes
