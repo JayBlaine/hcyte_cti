5e0bc20e26ccf5cfa427aec79a6cb06515e3dc93
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-02-23 15:49:43 -0600

    ffgfgg

--- app.py
+++ app.py
@@ -380,7 +380,7 @@ def build_visdcc(n_intervals=None, live_check=None, vis_filter=None, proto_filte
 #input: 
 #output: net, data
 @dash_app_micro.callback(
-    Input(component_id='net', component_property='selection')
+    [Input(component_id='net', component_property='selection')]
 )
 def display_sweeps_and_scans(scans):
     print(scans)
