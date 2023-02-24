872857eb0388e55cfd9ce458f175951d66a3480c
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-02-23 18:08:12 -0600

    bbbbb

--- app.py
+++ app.py
@@ -379,16 +379,17 @@ def build_visdcc(n_intervals=None, live_check=None, vis_filter=None, proto_filte
 
 #input: 
 #output: net, data
-#@dash_app_micro.callback(
-#    Output('nodes', 'children'),
-#    [Input(component_id='net', component_property='selection')]
-#)
-#def display_sweeps_and_scans(scans):
-#    print("running click function")
-    #if(len(scans['nodes']) > 0):
-    #    print("you clicked a node")
+@dash_app_micro.callback(
+    Output('nodes', 'children'),
+    [Input(component_id='net', component_property='selection')]
+)
+def display_sweeps_and_scans(scans):
+    print("running click function")
+    if(len(scans['nodes']) > 0):
+        print("you clicked a node")
+        print(scans['nodes'][0])
     #print("Printing clicked stuff: " + str(scans))
-#    return scans
+    return scans
     #print(selection)
 
 
