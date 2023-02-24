000583b3ab0307865b500b5dee5740935da91741
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-02-23 22:31:59 -0600

    sdfsdfsd

--- app.py
+++ app.py
@@ -405,7 +405,7 @@ def build_visdcc(n_intervals=None, live_check=None, vis_filter=None, proto_filte
 #input: 
 #output: net, data
 @dash_app_micro.callback(
-    Output(component_id='net', component_property='data'),
+    #Output(component_id='net', component_property='data'),
     Output(component_id='nodes', component_property='children'),
     Input(component_id='net', component_property='data'),
     Input(component_id='net', component_property='selection')
