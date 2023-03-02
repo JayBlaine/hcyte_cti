9747ea9e62f59df136116b957cf029bab27113fa
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-02 17:06:57 -0600

    sdff

--- app.py
+++ app.py
@@ -416,9 +416,9 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
     if(len(clicked_node['nodes']) > 0):
         print("you clicked a node: " + str(clicked_node['nodes'][0]))
         print("Scans dict values: " + str(scans_dict.values()))
-        #print("Scan nodes: " + str(scanNodes))
+        print("Scan nodes: " + str(scanNodes))
 
-        print("This is what the clicked node is scanning: " + str(scans_dict[clicked_node['nodes'][0]]))
+        #print("This is what the clicked node is scanning: " + str(scans_dict[clicked_node['nodes'][0]]))
 
         for scanned in scanNodes:
             if(scanned['id'] == scans_dict[clicked_node['nodes'][0]]):
