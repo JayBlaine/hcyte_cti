cf18acea4e6a190a5ec207ff53aa960720f50669
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-02 15:05:12 -0600

    fgfg

--- app.py
+++ app.py
@@ -420,7 +420,7 @@ def build_visdcc(n_intervals=None, live_check=None, vis_filter=None, proto_filte
 #output: net, data
 @dash_app_micro.callback(
     #Output(component_id='net', component_property='data'),
-    #Output(component_id='net', component_property='selection'),
+    Output(component_id='net', component_property='selection'),
     Input('net', 'data'),
     Input(component_id='net', component_property='selection'),
     prevent_initial_call=True
@@ -439,7 +439,7 @@ def display_sweeps_and_scans(net_data, clicked_node):
         #print(clicked_node['nodes'][0])
         for node in clicked_node['nodes']:
             print(node)
-        return net_data #, clicked_node
+        return clicked_node # net_data #, 
 
         #print("Scans: " + str(scans_dict))
         #print("Sweeps: " + str(sweeps_dict))
