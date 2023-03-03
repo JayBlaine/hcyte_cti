185185d60e197f2e916596483eb107bf72559fca
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-02 18:44:07 -0600

    bnbnvnnb

--- app.py
+++ app.py
@@ -436,10 +436,12 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
                 break
             
         if(fullNode["color"] == 'blue'):
+            print("Scan node clicked")
             for scanned in scanNodes:
                 if(scanned['id'] == scans_dict[clicked_node['nodes'][0]]):
                     nodes.append(scanned)
         if(fullNode["color"] == 'pink'):
+            print("Sweep node clicked")
             for swept in sweepNodes:
                 for edge in edges:
                     if(edge["from"] == clicked_node['nodes'][0] and edge["to"] == swept):
