b3c9a75e0de8832948355388ce20040e54c361f3
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-08 15:12:52 -0600

    safasfdsdf

--- app.py
+++ app.py
@@ -388,6 +388,7 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
             'title': "{}<br>number of flows: {}<br>malicious flows: {}<br>{}".format(ip_label, len(re.findall(ip + ':', ''.join(
                 list(visdcc_display_dict[active_int].keys())))), num_malicious, mal_alert_label)}
 
+        print("mal alert keys: " + str(mal_alerts.keys()))
         # ip filtering of nodes
         if new_node not in nodes and ip_type in vis_switches:
             # protocol filtering of nodes
