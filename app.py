ac7c38e6b12054bed11f4dab32d1ea106af6ddd7
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-23 13:32:17 -0500

    sdfsdf

--- app.py
+++ app.py
@@ -401,12 +401,12 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
             # protocol filtering of nodes
             if (num_udp > 0 and 'UDP' in proto_switches) or (num_tcp > 0 and 'TCP' in proto_switches):
                 if new_node["id"] in home_net:
-                    new_node["x"] = node_positions["homeNet"]
-                    new_node["y"] = node_positions["homeNet"]
+                    new_node["x"] = node_positions["homeNet"] + random.uniform(-50, 50)
+                    new_node["y"] = node_positions["homeNet"] + random.uniform(-50, 50)
                     print("{} is in the home network".format(new_node["id"]))
                 if new_node['id'] in home_ext:
-                    new_node["x"] = node_positions["homeExternal"]
-                    new_node["y"] = node_positions["homeExternal"]
+                    new_node["x"] = node_positions["homeExternal"] + random.uniform(-50, 50)
+                    new_node["y"] = node_positions["homeExternal"] + random.uniform(-50, 50)
                 if new_node['id'] in scans_dict.keys() and new_node['id'] in sweeps_dict.keys():
                     new_node['color'] = 'black'
                     nodes.append(new_node)
