0c2813df3e4d6f2fda7a96b4a730b42815661d62
jayblaine <$h@d0wb18DE>
2023-03-27 14:26:58 -0500

    init function for populating live micro line graph

--- app.py
+++ app.py
@@ -681,6 +681,7 @@ def pop_live_line_fig(flows:dict=None, y_ax:str='num_flows', interface:str=None)
     fig = px.line(
         data_frame=df, title='Live Flows: {} interface'.format(interface), hover_name='sec', hover_data=df.columns.tolist(), x='sec', y=y_ax).update_xaxes(
         rangeslider_visible=True).update_layout(height=300)#.update_traces(hovertemplate='%{y}<br>%{text}')
+    fig.add_scatter(x=df['sec'], y=df['alerts'])
     return fig
 
 
