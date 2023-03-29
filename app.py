1f55a8d11d698a0a691d011c0039dc1383bc9a94
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-29 12:33:33 -0500

    dfgdfgfg

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def create_dash_micro(flask_app):
                                                     'maxVelocity': 25,
                                                     'solver': 'barnesHut',
                                                     'barnesHut': {
-                                                        'gravitationalConstant': 0,
+                                                        'gravitationalConstant': -1,
                                                         'centralGravity': 0.00,
                                                         'springLength': 1000,
                                                         'springConstant': 0.01,
