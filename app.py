fcf1539a7114b160fe018f58615db7f8ec75c83b
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-03-29 12:35:32 -0500

    dfbvv

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def create_dash_micro(flask_app):
                                                         'gravitationalConstant': -100,
                                                         'centralGravity': 0.00,
                                                         'springLength': 1000,
-                                                        'springConstant': 0.01,
+                                                        'springConstant': 0.00,
                                             #            'damping': 0.09,
                                                         'avoidOverlap': 0.5
                                                     },
