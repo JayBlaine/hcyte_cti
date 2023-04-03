09277a84907b54d78bf36da8d8d7ccb20bf4295e
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-04-03 13:14:03 -0500

    askdfjsdf

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def create_dash_micro(flask_app):
                                  visdcc.Network(id='net',
                                                 #data=current_data,
                                                 selection = {'nodes':[], 'edges':[]},
-                                                options=dict(height='1200px', width='100%', layout={
+                                                options=dict(height='1200px', width='1200px', layout={
                                                     'improvedLayout': True,
                                                     'hierarchical': {
                                                         'enabled': False,
