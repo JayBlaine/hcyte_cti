27c7f3287ec2bb9d744f3dd1b82ab0584bdc072c
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-04-03 15:41:22 -0500

    mv,mcnv,cm

--- app.py
+++ app.py
@@ -438,7 +438,7 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
                     if new_node["id"] in home_net:
                         cluster_locations = new_node["id"].split(".")
                         print("CLuster locations: " + str(cluster_locations))
-                        position = calcCoordinates(int(cluster_locations[0])*10, int(cluster_locations[1])*10, 50)
+                        position = calcCoordinates(int(cluster_locations[0])*7, int(cluster_locations[1])*10, 50)
                         #position = calcCoordinates(node_positions['homeNet'][0], node_positions['homeNet'][1], 50)
                         new_node["x"] = position[0]
                         new_node["y"] = position[1]
@@ -465,7 +465,7 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
                     else:
                         cluster_locations = new_node["id"].split(".")
                         print("CLuster locations: " + str(cluster_locations))
-                        position = calcCoordinates(int(cluster_locations[0])*10, int(cluster_locations[1])*10, 50)
+                        position = calcCoordinates(int(cluster_locations[0])*7, int(cluster_locations[1])*10, 50)
                         #position = calcCoordinates(node_positions['other'][0], node_positions['other'][1], 50)
                         new_node["x"] = position[0]
                         new_node["y"] = position[1]
