4621019552bbbd78d2a034d5cda6bb83ed18ec85
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-04-07 11:22:20 -0500

    asfsdf

--- app.py
+++ app.py
@@ -483,8 +483,8 @@ def build_visdcc(clicked_node, n_intervals=None, live_check=None, vis_filter=Non
                 #    allScannedNodes = allScannedNodes + nodes
                 #print("All scanned nodes: " + str(allScannedNodes))
                 elif new_node['id'] in scans_dict.keys():
-                    #if new_node['id'] not in home_net:
-                    new_node['color'] = 'red'
+                    if new_node['id'] not in home_net:
+                        new_node['color'] = 'red'
                     new_node['title'] = new_node['title'] + "<br>Scanning Node"
                     nodes.append(new_node)
                     #scanNodes.append(new_node)
